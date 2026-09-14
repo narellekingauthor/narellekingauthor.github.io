982a533ef6d0bccb931194e060da1bb4660c092c
Narelle King <328552210+narellekingauthor@users.noreply.github.com>
2026-09-15 06:39:42 +1000

    Update nk-scripts.js

--- nk-scripts.js
+++ nk-scripts.js
@@ -1,5 +1,5 @@
 document.title='Narelle King Author'
 const buyFromPublisher = document.createElement('a')
-dloads = document.querySelectorAll('#header nav ul li.downloads')
+dloads = document.querySelectorAll('#header nav ul li.downloads a')
 dloads[0].textContent='Buy from Publisher'
 dloads[1].textContent='Buy from Amazon'
